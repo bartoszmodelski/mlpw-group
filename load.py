340355cabc0379e7c4a23e6e09bd8dc63bbdb399
model <modelski.bartosz@gmail.com>
2021-12-10 02:01:27 +0000

    simple exploration

--- load.py
+++ load.py
@@ -2,7 +2,7 @@ import pandas as pd
 from datetime import datetime
 from pprint import pprint
 
-date_parse = lambda dates: [datetime.strptime(d, '%m/%d/%Y %H:%M:%S %p') for d in dates]
+date_parse = lambda dates: [datetime.strptime(d, '%m/%d/%Y %I:%M:%S %p') for d in dates]
     
 
 def load_consumption_csv(filename):
